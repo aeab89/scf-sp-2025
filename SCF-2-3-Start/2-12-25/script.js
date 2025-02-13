a05af412c44b8fad187a78bd934522e02863aaf1
Andrea Arica <aeab89@gmail.com>
2025-02-12 19:27:49 -0500

    sum function

---
+++ SCF-2-3-Start/2-12-25/script.js
@@ -0,0 +1,9 @@
+//alert("Hello World");
+
+function CalculateTwoNumbers(){
+    let number1 = parseInt(document.getElementById("FirstNumber").value);
+    let number2 = parseInt(document.getElementById("SecondNumber").value);
+
+    alert(number1 + number2);
+}
+
