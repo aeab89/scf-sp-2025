80794df85d47047c54b0ed21252978500f7657c3
Andrea Arica <aeab89@gmail.com>
2025-02-12 20:19:24 -0500

    function recalling function

--- SCF-2-3-Start/2-12-25/script.js
+++ SCF-2-3-Start/2-12-25/script.js
@@ -3,7 +3,25 @@
 function CalculateTwoNumbers(){
     let number1 = parseInt(document.getElementById("FirstNumber").value);
     let number2 = parseInt(document.getElementById("SecondNumber").value);
+    let operationType = document.getElementById("OperationType").value;
 
-    alert(number1 + number2);
+    console.log (number1 * number2)
+
+    if(operationType == "Add"){
+        document.getElementById("Result").innerText = number1 + number2
+    }
+    else if(operationType == "Subtract"){
+        document.getElementById("Result").innerText = number1 - number2
+    }
+    else if(operationType == "Multiply"){
+        console.log (number1 * number2);
+        document.getElementById("Result").innerText = number1 * number2
+    }
+    else if(operationType == "Divide"){
+        document.getElementById("Result").innerText = number1 / number2
+    }
+
+    
+    
 }
 
