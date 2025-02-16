04e8e31665065aaa5ee3d0244b188bbe81da590f
Andrea Arica <aeab89@gmail.com>
2025-02-16 13:00:11 -0500

    calculator without alert

--- SCF-2-3-Start/2-12-25/script.js
+++ SCF-2-3-Start/2-12-25/script.js
@@ -5,8 +5,6 @@ function CalculateTwoNumbers(){
     let number2 = parseInt(document.getElementById("SecondNumber").value);
     let operationType = document.getElementById("OperationType").value;
 
-    console.log (number1 * number2)
-
     if(operationType == "Add"){
         document.getElementById("Result").innerText = number1 + number2
     }
@@ -20,8 +18,27 @@ function CalculateTwoNumbers(){
     else if(operationType == "Divide"){
         document.getElementById("Result").innerText = number1 / number2
     }
+}    
 
-    
-    
+/*let number1 = parseInt(document.getElementById("FirstNumber").value);
+let number2 = parseInt(document.getElementById("SecondNumber").value);
+
+function AddTwoNumbers(number1, number2){
+    let result = number1 + number2;
+    return result;
 }
 
+function SubtractTwoNumbers(number1, number2){
+    let result = number1 - number2;
+    return result;
+}
+
+function MultiplyTwoNumbers(number1, number2){
+    let result = number1 * number2;
+    return result;
+}
+
+function DivideTwoNumbers(number1, number2){
+    let result = number1 / number2;
+    return result;
+}*/
