e8fbb6cec21253027a2f9b1d12cdec4f3f65d3df
Andrea Arica <aeab89@gmail.com>
2025-03-08 00:38:33 -0500

    about page edited

---
+++ docs/scripts/aboutscript.js
@@ -0,0 +1,9 @@
+const navbar = document.getElementById("makemesticky");
+
+window.addEventListener("scroll", function() {
+    if (window.scrollY > 0) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+})
