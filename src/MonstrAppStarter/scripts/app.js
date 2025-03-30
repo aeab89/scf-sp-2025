78f5aac5d38fb6bdd7932397b7adf515c77a9043
Andrea Arica <aeab89@gmail.com>
2025-03-30 17:55:42 -0400

    added functionality to store selection

--- src/MonstrAppStarter/scripts/app.js
+++ src/MonstrAppStarter/scripts/app.js
@@ -2,7 +2,10 @@ const HomeSelectPanel = document.getElementById("SlotSelect");
 const ChoosePokemonPanel = document.getElementById("ChoosePokemon");
 const Slot1PokemonBtn = document.getElementById("Slot1");
 const Slot2PokemonBtn = document.getElementById("Slot2");
+const Slot1Container = document.getElementById("Slot1Selection");
+const Slot2Container = document.getElementById("Slot2Selection");
 const BacktoHomeBtn = document.getElementById("backButton");
+const SelectButton = document.getElementById("selectButton");
 const abilitiesUL = document.getElementById("abilities");
 const imageContainer = document.getElementById("image")
 const selectPokemon = document.getElementById("pokemonSelector");
@@ -10,33 +13,54 @@ const searchPokemon = document.getElementById("searchButton");
 const inputPokemon = document.getElementById("pokemonSearchBox");
 const statsContainer = document.createElement("div"); //New container for stats
 statsContainer.id = "statsContainer";
-if(ChoosePokemonPanel) {
-  ChoosePokemonPanel.appendChild(statsContainer);
-} //Appends statsContainer to the ChoosePokemonPanel div
-//document.body.appendChild(statsContainer); //Append it to the body
+ChoosePokemonPanel.appendChild(statsContainer); //Appends statsContainer to the ChoosePokemonPanel div
 
+let currentSlot;
+let currentSelection = {};
+let slot1Selection = {};
+let slot2Selection = {};
 
+//Event listeners to toggle between HomeSelectPanel and ChoosePokemonPanel
 Slot1PokemonBtn.addEventListener("click", () => {
-  ShowChoicePanel()
+  ShowChoicePanel(1)
 });
 
 Slot2PokemonBtn.addEventListener("click", () => {
-  ShowChoicePanel()
+  ShowChoicePanel(2)
 });
 
 BacktoHomeBtn.addEventListener("click", () => {
   ShowHomePanel()
 });
 
-function ShowHomePanel () {
-  HomeSelectPanel.style.visibility = 'visible';
-  ChoosePokemonPanel.style.visibility = 'hidden';
-}  
-
-function ShowChoicePanel () {
+//Function to hide HomeSelectPanel
+function ShowChoicePanel (slotNumber) {
+  currentSlot = slotNumber;
   HomeSelectPanel.style.visibility = 'hidden';
   ChoosePokemonPanel.style.visibility = 'visible';
 } 
+
+//Function to hide ChoosePokemonPanel and append sprite to HomeSelect panel
+function ShowHomePanel () {
+  HomeSelectPanel.style.visibility = 'visible';
+  ChoosePokemonPanel.style.visibility = 'hidden';
+  if(currentSlot == 1){
+    if(slot1Selection.sprite){ //appends sprite to slot 1 container after selection is made
+      let img = document.createElement("img");
+      img.src = slot1Selection.sprite;
+      Slot1Container.innerHTML = "";
+      Slot1Container.appendChild(img);
+    }
+  }
+  else if(currentSlot == 2){
+    if(slot2Selection.sprite){ //appends sprite to slot 2 container after selection is made
+      let img = document.createElement("img");
+      img.src = slot2Selection.sprite;
+      Slot2Container.innerHTML = "";
+      Slot2Container.appendChild(img);
+    }
+  }
+}  
 
 //Fetches list of pokemon names for the dropdown menu when page loads
 fetch('https://pokeapi.co/api/v2/pokemon/')
@@ -54,12 +78,12 @@ function BuildPokemonSelectOptions(pokemonOptions){
   });
 }
 
-//Event listener to fetch and display data when a pokemon is selected
+//Event listener to call fetch function when a pokemon is selected
 selectPokemon.addEventListener ("change", (event => {
   GetOnePokemon(event.target.value);
 }))
 
-//Event listener to fetch and display pokemon data when searched by name
+//Event listener to call fetch function when pokemon is searched by name
 searchPokemon.addEventListener ("click", (event => {
   event.preventDefault();
   const pokemonName = inputPokemon.value.trim().toLowerCase(); // Get user input, remove spaces, and turn to lowercase
@@ -69,16 +93,40 @@ searchPokemon.addEventListener ("click", (event => {
 }
 ))
 
+//Event listener to store current selection in slot 1
+SelectButton.addEventListener("click", () => {
+  slot1Selection = currentSelection;
+  slot2Selection = currentSelection;
+  ShowHomePanel();
+})
+
 //Function to fetch a pokemon's data based on its name
 function GetOnePokemon(pokemon){
 fetch('https://pokeapi.co/api/v2/pokemon/' + pokemon) //Fetch data from API
   .then(response => response.json()) //Convert response to JSON
   .then(data => {
-    //BuildAbilitiesList(data.abilities); //Populate abilities list
-    ShowSelectionImages(data.sprites); //Display pokemon image
+    //BuildAbilitiesList(data.abilities);
+    SetCurrentSelection(data);
+    ShowSelectionImages(data.sprites);
     DisplayStatsAndTypes(data.types, data.stats);
 })
   .catch(error => console.error('Error:', error)); // Catch and log any errors
+}
+
+//Function to store data in current selection object
+function SetCurrentSelection(data) {
+  currentSelection.sprite = data.sprites.front_default;
+  currentSelection.types = data.types;
+  currentSelection.stats = data.stats;
+  console.log(currentSelection);
+}
+
+//Function to display pokemon image
+function ShowSelectionImages(sprites){
+  imageContainer.innerHTML = ""; //Clear previous image
+  let image = document.createElement("img"); //Create image element
+  image.src = sprites.front_default; //Set image source to front sprite
+  imageContainer.appendChild(image); //Append image to container
 }
 
 //Function to build and display stats and types
@@ -120,13 +168,5 @@ function BuildAbilitiesList(abilities){
   li.innerHTML = element.ability.name; //Set list item text to ability name
   abilitiesUL.appendChild(li); //Append list item to UL
   });
-}
-
-//Function to display pokemon image
-function ShowSelectionImages(sprites){
-  imageContainer.innerHTML = ""; //Clear previous image
-  let image = document.createElement("img"); //Create image element
-  image.src = sprites.front_default; //Set image source to front sprite
-  imageContainer.appendChild(image); //Append image to container
 }
 
